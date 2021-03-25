02bcab7259a5cfebfa0951e9aa3472c472f180c3
Dian Prawira <dianprawira@sisfo.untan.ac.id>
2021-03-25 13:35:31 +0700

    autoload dan anonymous function

---
+++ application/Dosen.php
@@ -0,0 +1,32 @@
+<?php
+require_once("Pegawai.php");
+
+class Dosen extends Pegawai
+{
+    public $nidn;
+    public $jabatan_akademis;
+
+    function __construct($nip, $nama, $hp, $gaji, $nidn, $jabatan)
+    {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $hp;
+        $this->gaji_pokok = $gaji;
+        $this->nidn = $nidn;
+        $this->jabatan_akademis = $jabatan;
+    }
+
+    public function mengajar()
+    {
+        echo $this->nama . " sedang mengajar<br>";
+    }
+    public function meneliti()
+    {
+        echo $this->nama . " sedang meneliti<br>";
+    }
+
+    public function getNama()
+    {
+        return $this->nama;
+    }
+}
--- test_dosen.php
+++ test_dosen.php
@@ -1,8 +1,12 @@
 <?php
 
-require_once 'Dosen.php';
+require_once 'initialize.php';
+
 
 $dian = new Dosen('198411132015041001', 'Dian Prawira', '0800000', '50000000', '13118405', 'Tenaga Pengajar');
 
 $dian->mengajar();
 $dian->meneliti();
+
+$lita = new Mahasiswa('H1101191058', 'Lita Novitasari', '01-01-1965', 'P');
+$lita->tampilkanAngkatan();
